9aa5eb70b078dcc71c661d8f55eb323d1ba779c1
essalhi <khalid.essalhi@gmail.com>
2016-02-12 17:30:55 +0000

    dao + tests

---
+++ target/test-classes/org/adria/virementMultiple/dao/virementMultiple.java
@@ -0,0 +1,55 @@
+package org.adria.virementMultiple.dao;
+
+import org.adria.virementMultiple.entities.Beneficiary;
+import org.adria.virementMultiple.entities.Subscriber;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration("classpath:applicationContext.xml")
+
+@TransactionConfiguration(defaultRollback=false)
+@Transactional
+public class virementMultiple {
+
+	
+	
+	
+	@Autowired 
+	IBanqueDao banqueDao;
+	
+	@Test
+	public void findByUsernameTest(){
+		System.out.println(banqueDao.findByUsername("jira").getFirstName());
+	
+	}
+	
+	@Test
+	public void findBeneficiary(){
+		
+		Subscriber s=new Subscriber("jira", "ensa", "Atlas", "abdelghafor");
+		s.setId(new Long(1));
+		System.out.println(banqueDao.getAccountBySubscriber(s).size());
+	}
+	
+	@Test
+	public void findBeneficiaries(){
+		Subscriber s=new Subscriber("jira", "ensa", "Atlas", "abdelghafor");
+		s.setId(new Long(1));
+		System.out.println(banqueDao.getBeneficiaries(s).size());
+	}
+	
+	@Test
+	public void findBeneficiaryById(){
+		Beneficiary b=banqueDao.getBeneficiaryById(new Long(1));
+		System.out.println(b.getFirstName());
+	}
+
+}
